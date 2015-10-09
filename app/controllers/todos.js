3bdbe38384268c5a70bda5ebbcd8b154c0147376
Benjamin Fleischer <benjamin.fleischer@swipesense.com>
2015-10-09 17:01:29 -0500

    step

--- app/controllers/todos.js
+++ app/controllers/todos.js
@@ -1,13 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  model: function() {
-    var first  = {status_code: 1, project: "first"};
-    var second = {status_code: 1, project: "second"};
-    var third  = {status_code: 1, project: "third"};
-    var last   = {status_code: 2, project: "last"};
-    return [first, second, third, last];
-  }.property(),
   unassigned: function() {
     return this.get("model").filter(function(model) {
       return model.status_code === 1;
