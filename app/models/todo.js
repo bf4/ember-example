c74eab765662d15843a25a3fc829c46d9a645756
Benjamin Fleischer <benjamin.fleischer@swipesense.com>
2015-10-10 14:57:26 -0500

    step

--- app/models/todo.js
+++ app/models/todo.js
@@ -2,6 +2,7 @@ import Ember from "ember";
 
 export default Ember.Object.extend({
   status: function() {
-    return "Unassigned";
+    var status_code = this.get("status_code");
+    return status_code === 1 ? "Unassigned" : "Assigned";
   }.property()
 });
