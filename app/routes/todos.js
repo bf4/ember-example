90d89be74c2dc1d4595d2ab02599a25c5a2137c1
Benjamin Fleischer <benjamin.fleischer@swipesense.com>
2015-10-10 15:05:19 -0500

    step; ember test --server; failing test to attend; use Todo model

--- app/routes/todos.js
+++ app/routes/todos.js
@@ -1,11 +1,12 @@
 import Ember from "ember";
+import Todo from "my-kanban-board/models/todo";
 
 export default Ember.Route.extend({
   model: function() {
-    var first  = {status_code: 1, project: "first"};
-    var second = {status_code: 1, project: "second"};
-    var third  = {status_code: 1, project: "third"};
-    var last   = {status_code: 2, project: "last"};
+    var first  = Todo.create({status_code: 1, project: "first"});
+    var second = Todo.create({status_code: 1, project: "second"});
+    var third  = Todo.create({status_code: 1, project: "third"});
+    var last   = Todo.create({status_code: 2, project: "last"});
     return [first, second, third, last];
   }
 });
