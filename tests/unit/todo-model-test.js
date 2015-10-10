d433bd9f53328467be976a05e3ad028daa1baa1b
Benjamin Fleischer <benjamin.fleischer@swipesense.com>
2015-10-10 14:56:20 -0500

    step

--- tests/unit/todo-model-test.js
+++ tests/unit/todo-model-test.js
@@ -7,3 +7,8 @@ test('status_code of 1 is Unassigned', function(assert) {
   var subject = Todo.create({status_code: 1});
   assert.equal(subject.get("status"), "Unassigned");
 });
+
+test('status_code of 2 is Assigned', function(assert) {
+  var subject = Todo.create({status_code: 2});
+  assert.equal(subject.get("status"), "Assigned");
+});
