67b4a701fbec226e1cbbb4792034405e0fa2bda8
Benjamin Fleischer <benjamin.fleischer@swipesense.com>
2015-10-13 08:55:37 -0500

    step; inject currentRouteName

--- app/controllers/todos.js
+++ app/controllers/todos.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  application: Ember.inject.controller(),
+  isActiveRoute: Ember.computed.equal("application.currentRouteName", "todos.index"),
   actions: {
     assign: function(todo) {
       todo.set("status_code", 2);
